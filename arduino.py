288bfe8760640516858659fbe4de882fc42b7cdf
ecosort <justinw323@gmail.com>
2024-11-24 23:54:46 -0500

    updates

--- arduino.py
+++ arduino.py
@@ -1,8 +1,9 @@
 import serial
-import random
 import time
 import os
 import detect_script as script
+import live_capture as capture
+import send_data 
 
 port="/dev/ttyACM0"
 
@@ -14,12 +15,14 @@ while not done:
 	if data:
 		print(data)
 		if data == b'\x01':
+			# capture.show_camera()
 			os.system("gst-launch-1.0 nvarguscamerasrc num-buffers=1 sensor_id=0 ! 'video/x-raw(memory:NVMM), width=4608, height=2592, framerate=14/1, format=NV12' ! nvjpegenc ! filesink location=image.jpg")
-			num = script.detect("image.jpg")
-			print(num)
-			arduino.write(str(int(num)).encode())
+			label, isRecyclable = script.detect("capture.jpg")
+			arduino.write(str(int(isRecyclable)).encode())
 			arduino.flush()
 			done = True
+			#send_data.get_data(label, isRecyclable)
+			print(label, isRecyclable)
 	else:
 		print("no data")
 	time.sleep(5)
